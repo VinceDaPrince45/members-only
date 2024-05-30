788c6b7f4dbb66d4de1c33685674f00003d734eb
Vincent <Vincentnguyen030@gmail.com>
2024-05-30 01:16:04 -0500

    Set up passport

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require("passport-local").Strategy;
 const indexRouter = require('./routes/index');
+const User = require('./models/User');
 const Schema = mongoose.Schema;
 
 mongoose.connect(process.env.MONGO_URI);
@@ -21,6 +22,34 @@ app.set("view engine", "ejs");
 app.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            const user = await User.findOne({username:username}).exec();
+            if (!user) {
+                return done(null,false, {message: "Incorrect username"});
+            }
+            const isMatch = await bcrypt.compare(password,user.password);
+            if (!isMatch) {
+                return done(null,false, {message: "Incorrect password."});
+            }
+            return done(null,user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+passport.serializeUser((user,done) => {
+    done(null,user.id);
+});
+passport.deserializeUser(async (id,done) => {
+    try {
+        const user = await User.findById(id);
+        done(null,user);
+    } catch (err) {
+        done(err)
+    }
+});
 
 app.get("/", (req, res) => res.render("index"));
 
